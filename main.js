d51793c58882993d06cfa02a419990204995a6e3
unknown <parvezmosharraf.official@gmail.comm>
2022-06-28 19:59:51 +0600

    some changes

--- main.js
+++ main.js
@@ -2,10 +2,9 @@
 // execute script after page load 
 window.onload = function digital_fn(){
     // toggle button 
-    let toggle = document.querySelector('toggle-btn')
-    let collapse = document.querySelector('collapse')
-
-    toggle.addEventListener('click', function(){
-        
+    let toggle = document.querySelector('.toggle-btn')
+    let collapse = document.querySelector('.collapse')
+    toggle.addEventListener('click',function(event){
+        collapse.classList.toggle('active')
     })
 }
