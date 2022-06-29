d1682469c09b4d76b094f016462541febb64c32a
unknown <parvezmosharraf.official@gmail.comm>
2022-06-29 16:16:00 +0600

    some changes

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+// way no 01
 
-// execute script after page load 
-window.onload = function digital_fn(){
-    // toggle button 
-    let toggle = document.querySelector('.toggle-btn')
-    let collapse = document.querySelector('.collapse')
-    toggle.addEventListener('click',function(event){
-        collapse.classList.toggle('active')
-    })
-}
+// // execute script after page load 
+// window.onload = function digital_fn(){
+//     // toggle button 
+//     let toggle = document.querySelector('.toggle-btn')
+//     let collapse = document.querySelector('.collapse')
+//     toggle.addEventListener('click',function(event){
+//         collapse.classList.toggle('active')
+//     })
+// }
+
+// way no 02 
+
+const btn = document.querySelector('.toggle-btn');
+const nav = document.querySelector('.collapse');
+btn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+})
+
